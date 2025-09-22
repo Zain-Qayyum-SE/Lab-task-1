a488ea14d4f32514a94300bc34e8ab3e711d1d59
Zain-Qayyum-SE <hassanqayyum145175@gmail.com>
2025-09-22 13:46:41 +0500

    Add files via upload

---
+++ Python Code/Task 1 .py
@@ -0,0 +1,9 @@
+
+name = "zain"
+age = 18 
+height = 5.7
+is_false = False 
+print("Name :" , name )
+print("Height  :" , height )
+print("Age  :" , age )
+print("Student  :" , is_false  )
---
+++ Python Code/Task 2.py
@@ -0,0 +1,10 @@
+x = 2
+y = 13
+print(" Addition :" , x+y )
+print(" Subtraction  :" , x-y )
+print(" Multiplication  :" , x*y )
+print(" Division :" , x/y )
+print(" Floor Division :" , x//y )
+print(" Exponential  :" , x**y )
+print(" Modulus  :" , x%y )
+
